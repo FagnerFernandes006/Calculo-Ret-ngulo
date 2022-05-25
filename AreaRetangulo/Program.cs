671dbda13fe6735d874587d204a750fc74d894c5
Fagner Fernandes <Fagner Fernandes@DESKTOP-ACNLJLV>
2022-05-24 21:59:49 -0300

    Calculo com Retângulo

---
+++ AreaRetangulo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+namespace AreaRetangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo retangulo = new Retangulo();
+
+            Console.WriteLine("Entre com as medidas do retângulo: ");
+            Console.Write("Base: ");
+            retangulo.Base = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Altura: ");
+            retangulo.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double area = retangulo.Area();
+            double perimetro = retangulo.Perimetro();
+            double diagonal = retangulo.Diagonal();
+            Console.WriteLine();
+            Console.WriteLine("Área: " + area.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Perímetro: " + perimetro.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Diagonal: " + diagonal.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
---
+++ AreaRetangulo/Retangulo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaRetangulo
+{
+    public class Retangulo
+    {
+        public double Base { get; set; }
+        public double Altura { get; set; }
+
+        public double Area()
+        {
+            return Base * Altura;
+        }
+        public double Perimetro()
+        {
+            return (Base + Base) + (Altura + Altura);
+        }
+        public double Diagonal()
+        {
+            return Math.Sqrt(Math.Pow(Base, 2) + Math.Pow(Altura, 2));
+        }
+    }
+}
